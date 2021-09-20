3b6cb7cc6017401664281c953833d34246c15609
cuixiaorui <cui_xiaorui@126.com>
2021-09-20 18:40:03 +0800

    add 03-used-jsx

---
+++ 03/README.md
@@ -0,0 +1,4 @@
+# 使用 jsx
+
+配置  @vitejs/plugin-vue-jsx 插件
+
---
+++ 03/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import User from "./components/User.jsx";
+</script>
+
+<template>
+  <User></User>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
